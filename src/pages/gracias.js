8fc29f0ab5e1163a52063f67c93d9225c5c85af6
Luis Antonio Rodriguez Garcia <luis_rdz5@hotmail.com>
2020-07-08 00:22:32 -0500

    added image sharp to the project

--- src/pages/gracias.js
+++ src/pages/gracias.js
@@ -11,7 +11,7 @@ export default function gracias() {
         <h2>Compra Exitosa!</h2>
         <p>Espero que disfrutes tu swag, lucelo con orgullo</p>
         <p>¡Te esperamos de vuelta, no pares de aprender!</p>
-        <span rol="img" aria-label="emoji">
+        <span role="img" aria-label="emoji">
           {" "}
           💚{" "}
         </span>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,38 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { Jumbo } from "../components"
 import { SEO } from "../components"
 
-const IndexPage = () => (
-  <>
-    <SEO title="Home" />
-    <Jumbo />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/gracias/">Go to gracias</Link>
-    <Link to="/cancelacion/">Go to cancelacion</Link>
-  </>
-)
+export const query = graphql`
+  query GET_DESCRIPTION {
+    allSite {
+      edges {
+        node {
+          siteMetadata {
+            description
+          }
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => {
+  console.log()
+
+  return (
+    <>
+      <SEO title="Home" />
+      <Jumbo
+        description={data.allSite.edges[0].node.siteMetadata.description}
+      />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/gracias/">Go to gracias</Link>
+      <Link to="/cancelacion/">Go to cancelacion</Link>
+    </>
+  )
+}
 
 export default IndexPage
