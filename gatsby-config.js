609c2f57937ae20c314237510cd21763899ceeb2
Luis Antonio Rodriguez Garcia <luis_rdz5@hotmail.com>
2020-07-08 21:00:02 -0500

    modified the dotenv configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,8 @@
-require("dotenv").config()
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
 
 module.exports = {
   siteMetadata: {
